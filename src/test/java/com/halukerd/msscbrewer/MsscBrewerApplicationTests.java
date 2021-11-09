630bcaf12e3d889ad2ae794d44533ca5fd4e20c2
halukerd <erd.haluk@gmail.com>
2021-11-09 23:40:29 +0300

    Initial commit

---
+++ src/test/java/com/halukerd/msscbrewer/MsscBrewerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.halukerd.msscbrewer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBrewerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
